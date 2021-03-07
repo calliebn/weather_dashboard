43498f080b72b7bf5123ab7d299822526515fd27
Callie Nipper <callie.nipper@gmail.com>
2021-03-06 17:23:44 -0800

    Added image folder and screenshots. Worked on CSS

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,91 +1,88 @@
 let cities = [];
 let apiKey = '745cbb713a047695c07ef19d3aefc656'
 
-let cityFormEl = document.querySelector("#city-search");
 let cityInputEl = document.querySelector("#city");
-let weatherContainerEl = document.querySelector("#current-weather");
 let cityNameEl = document.querySelector("#cityName");
 let fiveDayContainerEl = document.querySelector("#five-day")
-let pastSearchButtonEl = document.querySelector("#past-search")
 
-let formSubmitHandler = function (event) {
+function getCityWeather(event) {
     event.preventDefault();
     let city = cityInputEl.value.trim();
-    if (city) {
-        getCityWeather(city);
-        get5Day(city);
-        cities.unshift({ city });
-        cityInputEl.value = "";
-    } else {
-        alert("Please enter a city");
-    }
-    saveSearch();
-    pastSearch();
-}
-
-let saveSearch = function () {
-    localStorage.setItem("cities", JSON.stringify(cities));
-};
-
-let getCityWeather = function (event) {
-    event.preventDefault();
-    let city = cityInputEl.value.trim();
-    let apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-    console.log(apiURL)
+    let apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    console.log(apiURL);
 
     fetch(apiURL)
         .then(function (response) {
-            response.json().then(function (data) {
+            return response.json().then(function (data) {
                 console.log("Current Weather", data);
                 // displaying Current Weather
-                const milliseconds = data.dt * 1000
-                const dateObject = new Date(milliseconds)
-                const humanDate = dateObject.toLocaleString("en-US", { month: "long", day: "numeric", year: "numeric" })
+                const milliseconds = data.dt * 1000;
+                const dateObject = new Date(milliseconds);
+                const humanDate = dateObject.toLocaleString("en-US", { month: "long", day: "numeric", year: "numeric" });
 
+                document.querySelector(".five-header").classList.remove("hide");
                 cityNameEl.textContent = data.name;
-                document.getElementById("date").textContent = humanDate
-                document.getElementById("icon").setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
-                document.getElementById("temperature").textContent = "Temperature: " + data.main.temp + "ºF"
-                document.getElementById("humidity").textContent = "Humidity: " + data.main.humidity + "%"
-                document.getElementById("windSpeed").textContent = "Wind Speed: " + data.wind.speed + " MPH"
-                let lat = data.coord.lat
-                let lon = data.coord.lon
-                getForecastWeather(lat, lon)
+                document.getElementById("date").textContent = humanDate;
+                document.getElementById("icon").setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+                document.getElementById("temperature").textContent = "Temperature: " + data.main.temp + "ºF";
+                document.getElementById("humidity").textContent = "Humidity: " + data.main.humidity + "%";
+                document.getElementById("windSpeed").textContent = "Wind Speed: " + data.wind.speed + " MPH";
+                let lat = data.coord.lat;
+                let lon = data.coord.lon;
+                getForecastWeather(lat, lon);
             });
         });
-};
+}
 
 let getForecastWeather = function (lat, lon) {
     let forecastURL = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${apiKey}`
 
     fetch(forecastURL)
         .then(function (response) {
-            response.json().then(function (data) {
+            return response.json().then(function (data) {
                 console.log("Forecast Weather", data);
+                let uvIndex = $("#uvIndex")
                 document.getElementById("uvIndex").textContent = "UV Index: " + data.current.uvi;
-                if (data.current.uvi.value <= 2) {
-                    document.getElementById("uvIndex").addClass("low")
-                } else if (data.current.uvi.value > 2 && data.current.uvi.value < 8) {
-                    addClass("med");
-                } else if (data.current.uvi.value > 8) {
-                    addClass("high")
+                if (data.current.uvi < 2) {
+                    uvIndex.addClass("low")
+                } else if (data.current.uvi > 2 && data.current.uvi < 8) {
+                    uvIndex.addClass("med");
+                } else if (data.current.uvi > 8) {
+                    uvIndex.addClass("high")
                 }
                 //Creating 5-day forecast cards
-                fiveDayContainerEl = data
+                let fiveForecastEl = $(".five-forecast")
+                fiveForecastEl.text("")
                 for (let i = 1; i < 6; i++) {
                     //Changing date from UNIX date to Human Date
                     const milliseconds = data.daily[i].dt * 1000
                     const dateObject = new Date(milliseconds)
                     const humanDate = dateObject.toLocaleString("en-US", { month: "long", day: "numeric", year: "numeric" })
 
-                    //Creating cards
-                    let newDayCardEL = $("<div>")
-                    newDayCardEL.addClass("card")
+                    //Creating the daily cards
+                    let newDayCardEl = $("<div>")
+                    newDayCardEl.addClass("col-2 col-md-2 col-sm-12 card text-center five-day");
+                    newDayCardEl.appendTo(fiveForecastEl)
+                    let newCardBodyEl = $("<div>")
+                    newCardBodyEl.addClass("card-body");
+                    newCardBodyEl.appendTo(newDayCardEl)
+                    let cardH3El = $("<h3>")
+                    cardH3El.addClass("card-title five-date");
+                    cardH3El.text(humanDate)
+                    cardH3El.appendTo(newCardBodyEl)
+                    let cardIconEl = $("<img>")
+                    cardIconEl.addClass("five-img");
+                    cardIconEl.attr("src", "http://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png")
+                    cardIconEl.appendTo(newCardBodyEl)
+                    let cardTempEl = $("<p>")
+                    cardTempEl.addClass("card-text five-temp");
+                    cardTempEl.text(data.daily[i].temp.day + "ºF")
+                    cardTempEl.appendTo(newCardBodyEl)
+                    let cardHumidEl = $("<p>")
+                    cardHumidEl.addClass("card-text five-humid");
+                    cardHumidEl.text(data.daily[i].humidity + "%")
+                    cardHumidEl.appendTo(newCardBodyEl)
 
-                    document.getElementById("five-date").textContent = humanDate
-                    //document.getElementById("five-img").setAttribute("src", "http://openweathermap.org/img/w/" + data.daily[i].weather.icon + ".png")
-                    document.getElementById("five-temp").textContent = data.daily[i].temp.day + "ºF"
-                    document.getElementById("five-humid").textContent = data.daily[i].humidity
                 }
             })
         })
